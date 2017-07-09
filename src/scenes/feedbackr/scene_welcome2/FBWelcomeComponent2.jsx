519f1b6cbf9f11c0c1239f1a9537ab8dceb7d84d
Tom Beaver <sourcetree@tombeaver.com>
2017-07-09 16:32:48 -0500

    List is is looking great, but DropDownMenu doesn't work on this page. It works a little on lab page

--- src/scenes/feedbackr/scene_welcome2/FBWelcomeComponent2.jsx
+++ src/scenes/feedbackr/scene_welcome2/FBWelcomeComponent2.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Grid, Row, Col, Button, ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
 import ReactTimestamp from 'react-timestamp'
 import { ListGroup } from "react-bootstrap";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -7,24 +6,36 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // material-ui
 import {List, ListItem} from 'material-ui/List';
 import {Avatar} from 'material-ui'
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+//import Divider from 'material-ui/Divider';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
 
-import Divider from 'material-ui/Divider';
+
+// colors
+import {pinkA200, blue600, red600, transparent} from 'material-ui/styles/colors';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+
 import IconChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
 import IconMoreVert from 'material-ui-icons/MoreVert'
 import IconMusicNote from 'material-ui-icons/MusicNote'
-import IconFace from 'material-ui-icons/TagFaces'
+import IconTagFace from 'material-ui-icons/TagFaces'
 import IconThumbDown from 'material-ui-icons/ThumbDown'
 import ActionGrade from 'material-ui/svg-icons/action/grade';
 
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Chip from 'material-ui/Chip';
+
 //import '../css/feedbackr.css'
 import '../css/fbWelcome2b.css'
-import '../css/listMod.css'
 import '../../../app/pills.css'
 import '../../../app/fonts/kudos_fonts.css'
 //import { setDomainFilter } from '../actions/setDomainFilter'
 
-// colors
-import {pinkA200, blue600, red600, transparent} from 'material-ui/styles/colors';
+// For tap events, such as right-icon menu
+import InjectTapEventPlugin from 'react-tap-event-plugin'
 
 export default class FBWelcomeComponent2 extends React.Component {
 
@@ -42,14 +53,21 @@ export default class FBWelcomeComponent2 extends React.Component {
 
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleDomainDropdown = this.handleDomainDropdown.bind(this);
+		//this.handleTopicTap = this.handleTopicTap.bind(this);
   }
 
 
   componentDidMount() {
-		console.log("componentDidMount");
 		this.props.setDomainFilter();
     this.props.getFeedback();
+		console.log("WILL INJESCT TAP EVENT PLUGIN");
+		InjectTapEventPlugin();
   }
+
+	handleTopicTap(value) {
+		console.log("handleTopicTap");
+		console.log("value:", value);
+	}
 
 	// onChange={e => this.setState({ name: e.target.value })}
 	handleInputChange(event) {
@@ -60,12 +78,16 @@ export default class FBWelcomeComponent2 extends React.Component {
 	}
 
 	handleDomainDropdown(key, event) {
-		console.log("handleDomainDropdown:");
 		this.props.setDomainFilter(key)
+	}
+
+	handleDropDownChange() {
+		console.log("handleDropDownChange");
 	}
 
   render() {
 		console.log("--------------------------------------------------");
+
 		let { domains, topics, users, opinions } = this.props.feedbackObj;
 		let { domainFilter } = this.props.domainFilterObj;
 
@@ -80,38 +102,69 @@ export default class FBWelcomeComponent2 extends React.Component {
 			});
 		}
 
+
+		const rightIconElement = (
+			<IconButton
+				touch={true}
+			>
+				<IconMoreVert color={grey400} />
+			</IconButton>
+		)
+
+		const rightIconMenu = (
+			<IconMenu iconButtonElement={rightIconElement}>
+				<MenuItem>Reply</MenuItem>
+				<MenuItem>Forward</MenuItem>
+				<MenuItem>Delete</MenuItem>
+			</IconMenu>
+		)
+
+
     return (
       <div id="returnBlock">
 
 
+					<DropDownMenu value="LAURAMYLVOE" onChange={this.handleDropDownChange}  openImmediately={true}>
+						<MenuItem value={1} primaryText="Never"/>
+						<MenuItem value={2} primaryText="EveryNight" />
+						<MenuItem value={3} primaryText="Weeknights" />
+						<MenuItem value={4} primaryText="Weekends" />
+						<MenuItem value={5} primaryText="Weekly" />
+					</DropDownMenu>
+
         {/* ----- material dynamic ----- */}
 
+
         <div>
-					<h2>DYNAMIC OPINIONS</h2>
+					<h2>Opinions</h2>
 					{opinions && opinions.length > 0 ? (
-						<List >
+						<List className="opinionList">
 							{opinions.map((opinion, index) => {
 								return (
 									<div>
+										<p className="opinionList-divider"></p>
 
-									<ListItem
-										leftIcon={<ActionGrade color={pinkA200} />}
-										primaryText={
-												<div>
-													<p>{opinion.body + opinion.body + opinion.body}</p>
-													<p>{"— Elizabeth Montgomery"}</p>
+										<ListItem className="opinionList-item"
+											disabled
+											leftIcon={<IconTagFace color={pinkA200} />}
+											rightIconButton={rightIconMenu}
+											primaryText={
+													<div className="opinionList-primary">
+														<div className="opinionList-primary-opinion">{opinion.body}</div>
+														<FlatButton style={{ fontSize: '16px' }} primary>{"Elizabeth Montgomery, Brooklyn NY"}</FlatButton>
 
+													</div>
+												}
+											secondaryText={
+												<div className="opinionList-secondary">
+													<Chip className="opinionList-secondary-item left"
+														onClick={this.handleTopicTap.bind(this, opinion.topic)}>{opinion.topic}</Chip>
+													<Chip className="opinionList-secondary-item right" backgroundColor={"#ffffff"} labelColor={"#aaaaaa"}>6 days ago</Chip>
 												</div>
 											}
-										secondaryText={
-											<div className="opinionListListItemFooter">
-												<span className="footerItem left">{opinion.topic}</span>
-												<span className="footerItem center">6 days ago</span>
-											</div>
-										}
-										rightIcon={<IconChevronRight />} />
-									</div>
 
+										/>
+									</div>
 
 
 								);
@@ -120,6 +173,10 @@ export default class FBWelcomeComponent2 extends React.Component {
 						</List>
 
           ) : <p>NO OPINIONS</p>}
+
+
+
+
 
 
         </div>
