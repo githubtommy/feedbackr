b88a580b489bc72f3255f6aa42358f066917842c
Tom Beaver <sourcetree@tombeaver.com>
2017-07-10 20:22:39 -0500

    modify App component to be a class and have its own render method, preparatory to adding slide transitions

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -10,26 +10,36 @@ import Bio from '../scenes/bio/bio.jsx'
 import Lab from '../scenes/lab/lab.jsx'
 import WeKnow from '../scenes/weknow/WeKnowContainer.jsx'
 
-const App = () => (
-  <div className="container">
-    <header>
-			<br />
-      <Link to="/">Home</Link> | <Link to="/fbwelcome2">Welcome2</Link> | <Link to="/fbhome">Feedbackr Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/about">About</Link> | <Link to="/bio">Bio</Link> | <Link to="/lab">Lab</Link> | <Link to="/weknow">Things We Think We Know</Link>
-			<br />
-			<br />
-    </header>
+class App extends React.Component {
+	render() {
 
-    <main>
-      <Route exact path="/" component={FBWelcome} />
-      <Route exact path="/fbwelcome2" component={FBWelcome2} />
-      <Route exact path="/fbhome" component={FBHome} />
-      <Route exact path="/counter" component={Counter} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/bio" component={Bio} />
-      <Route exact path="/lab" component={Lab} />
-      <Route exact path="/weknow" component={WeKnow} />
-    </main>
-  </div>
-)
+		return (
+
+			<div className="container">
+				<header>
+					<br />
+					<Link to="/">Home</Link> | <Link to="/fbwelcome2">Welcome2</Link> | <Link to="/fbhome">Feedbackr Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/about">About</Link> | <Link to="/bio">Bio</Link> | <Link to="/lab">Lab</Link> | <Link to="/weknow">Things We Think We Know</Link>
+					<br />
+					<br />
+				</header>
+
+				<main>
+					<Route exact path="/" component={FBWelcome} />
+					<Route exact path="/fbwelcome2" component={FBWelcome2} />
+					<Route exact path="/fbhome" component={FBHome} />
+					<Route exact path="/counter" component={Counter} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/bio" component={Bio} />
+					<Route exact path="/lab" component={Lab} />
+					<Route exact path="/weknow" component={WeKnow} />
+				</main>
+			</div>
+		)
+	}
+}
+
+//const App = () => (
+//
+//)
 
 export default App
